8504a4d70e4a50c1bb164ee845b57705af44b024
Govind K <nh45043@hertz.net>
2020-03-28 14:55:19 -0700

    Initial Commit

---
+++ config.js
@@ -0,0 +1,48 @@
+
+
+
+// CAROUSEL CONFIG OBJECT
+// ----------------------
+
+	var carousel = {
+
+		// THE AMOUNT OF TIME TO DELAY BETWEEN AUTOMATICALLY CYCLING AN ITEM. IF FALSE, CAROUSEL WILL NOT AUTOMATICALLY CYCLE
+		// -------------------------------
+		// INTERVAL : INTEGER [ 0 - 2147483647 ] | BOOLEAN[ false ]
+		interval: 2000,
+
+
+
+		// WHETHER THE CAROUSEL SHOULD REACT TO KEYBOARD EVENTS
+		// ----------------------------------------------------
+		// KEYBOARD : BOOLEAN [ true | false ]
+
+		keyboard: false,
+
+
+
+		// IF SET TO "HOVER", PAUSES THE CYCLING OF THE CAROUSEL ON MOUSEENTER AND RESUMES THE CYCLING OF THE CAROUSEL ON MOUSELEAVE
+		// IF SET TO NULL, HOVERING OVER THE CAROUSEL WON'T PAUSE IT.
+		// -------------------------------------------------------------------------------------------------------------------------
+		// PAUSE : STRING[ 'hover'] | null
+
+		pause: null,
+
+
+
+		// AUTOPLAYS THE CAROUSEL AFTER THE USER MANUALLY CYCLES THE FIRST ITEM. IF "CAROUSEL", AUTOPLAYS THE CAROUSEL ON LOAD
+		// --------------------------------------------------------------------------------------------------------------------
+		// RIDE : STRING[ 'carousel' ] | BOOLEAN[ false ]
+
+		ride: false,
+
+
+
+		// WHETHER THE CAROUSEL SHOULD CYCLE CONTINUOUSLY OR HAVE HARD STOPS
+		// -----------------------------------------------------------------
+		// WRAP : BOOLEAN [ true | false ]
+
+		wrap: true
+
+	}
+
